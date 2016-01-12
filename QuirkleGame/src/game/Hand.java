83973a83436dee92f1ba81e7565071d409162573
MrFix93 <pjotrwes@hotmail.com>
2016-01-12 15:18:49 +0100

    Created Stubbs for the class Hand

--- QuirkleGame/src/game/Hand.java
+++ QuirkleGame/src/game/Hand.java
@@ -1,5 +1,97 @@
 package game;
 
-public class Hand {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Hand {
+	private List<Tile> tilesInHand = new ArrayList<Tile>();
+	private Player ownerPlayer;
+	private static final int LIMIT = 6;
+	
+	/**
+	 * Constructor. Sets this.ownerPlayer to ownerPlayer.
+	 * @param ownerPlayer
+	 */
+	public Hand(Player ownerPlayer) {
+		
+	}
+	
+	/**
+	 * Reset the hand: it removes all the tiles an ensures that current hand is empty.
+	 * @return boolean, true if succesful
+	 */
+	public boolean resetHand() {
+		return true;
+	}
+	
+	/**
+	 * Adds a single tile-object to the player's hand to a random empty place.
+	 * @param tile The tile-object that needs to be added to the hand.
+	 * @return boolean True if succesful added.
+	 */
+	
+	public boolean addToHand(Tile tile) {
+		// TODO: Implement addTileToHand
+		return true;
+	}
+	
+	/**
+	 * Adds multiple tiles to the hand.
+	 * @param tileArray An array with tile-object that needs to be added to the hand.
+	 * @return boolean True if succesful
+	 */
+	
+	public boolean addTohand(List<Tile> tileList) {
+		return true;
+	}
+	
+	/**
+	 * Removes a tile object from hand. In case two identical tiles are in hand, one will be randomly removed.
+	 * @param The tile-object that needs to be removed from the hand. 
+	 * @return true if succesful
+	 */
+	
+	public boolean removeFromHand(Tile tile) {
+		// TODO: Implement removeTileFromHand 
+		return true;
+	}
+	
+	/**
+	 * Removes multiple tiles from hand
+	 * @param tileList The list of tiles that needs to be removed.
+	 * @return true if succesful
+	 */
+	
+	public boolean removeFromHand(List<Tile> tileList) {
+		// TODO: Implement removeTileFromHand 
+		return true;
+	}
+	
+	/**
+	 * Returns the tiles that are currently in hand.
+	 * @return An array with tile-objects.
+	 */
+	
+	public Tile[] getTilesInHand() {
+		return null;
+	}
+	
+	/**
+	 * Get the owner of the hand
+	 * @return Player object
+	 */
+	public Player getOwnerOfHand() {
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @return Returns the amount of tiles that are currently in hand.
+	 */
+	
+	public int getAmountOfTiles() {
+		return 0;
+	}
+	
+	
 }
