9991ac910aa59da2df6eff1f46acbb8a1d090bfe
Peter <Peter@Peter>
2016-01-13 11:34:48 +0100

    Added Move and Swap classes

--- QuirkleGame/src/game/Move.java
+++ QuirkleGame/src/game/Move.java
@@ -1,5 +1,15 @@
 package game;
 
-public class Move {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Move {
+	public Tile tileToPlay;
+	public BoardSquare position;
+	
+	public Move(Tile tile, BoardSquare boardsquare) {
+		this.tileToPlay = tile;
+		this.position = boardsquare;
+	}
+	
 }
