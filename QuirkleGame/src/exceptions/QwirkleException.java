29b85bc369e8756a51a4074b5311b793e8fb6750
Jonathan Juursema <github@jonathanj.nl>
2016-01-13 16:51:53 +0100

    Initial exceptions added.

---
+++ QuirkleGame/src/exceptions/QwirkleException.java
@@ -0,0 +1,14 @@
+package exceptions;
+
+/**
+ * A general exception for all things related to the game. All our exceptions will extend this one.
+ * This will make it easier to differentiate between game rule violations and actual programming
+ * errors.
+ * 
+ * @author Jonathan Juursema & Peter Wessels
+ *
+ */
+@SuppressWarnings("serial")
+public class QwirkleException extends Exception {
+
+}
---
+++ QuirkleGame/src/exceptions/SquareOutOfBoundsException.java
@@ -0,0 +1,27 @@
+package exceptions;
+
+/**
+ * This exception is thrown when a square outside the game board is being accessed.
+ * 
+ * @author Jonathan Juursema & Peter Wessels
+ *
+ */
+@SuppressWarnings("serial")
+public class SquareOutOfBoundsException extends QwirkleException {
+
+	int index, max, min;
+	char vector;
+	
+	public SquareOutOfBoundsException(int index, int max, char vector) {
+		this.index = index;
+		this.max = max / 2;
+		this.min = (this.max - 1) * -1;
+		this.vector = vector;
+	}
+	
+	public String getMessage() {
+		return "The " + this.vector + " coordinate is out of bounds. "
+				+ "Requested " + this.index + " but should be " + this.min + ".." + this.max + ".";
+	}
+
+}
