03069c2b707aa12080c2348e40d8026ebc5f8929
Jonathan Juursema <github@jonathanj.nl>
2016-01-13 16:52:33 +0100

    Implementation of Board and BoardSquare.

--- QuirkleGame/src/game/Board.java
+++ QuirkleGame/src/game/Board.java
@@ -1,11 +1,21 @@
 package game;
 
-import java.util.List;
+import exceptions.SquareOutOfBoundsException;
 
+/**
+ * A class representing a game board, used by the Game class. The board is represented as
+ * a native 2D array of custom objects. We use a fixed array because of the relative small size
+ * of the array and to keep things simplified. All public methods of this class working with 
+ * coordinates assume board coordinates, allowing for negative integers. Internally, these are
+ * converted back to array coordinates starting at 0.
+ * 
+ * @author Jonathan Juursema & Peter Wessels
+ *
+ */
 public class Board {
 	
 	private Game game;
-	private List<List<BoardSquare>> board;
+	private BoardSquare[][] board;
 	
 	/**
 	 * Initializes a game board for a given game.
@@ -13,47 +23,96 @@ public class Board {
 	 */
 	public Board(Game game) {
 		this.game = game;
-		// TODO Implement constructor.
+		int arraySize = 2 * this.game.getTilesPerType()
+						* Game.DIFFERENTCOLORS * Game.DIFFERENTSHAPES;
+		this.board = new BoardSquare[arraySize][arraySize];
+		for (int i = 0; i < arraySize; i++) {
+			for (int j = 0; j < arraySize; j++) {
+				this.board[i][j] = new BoardSquare(this, i, j);
+			}
+		}
+	}
+	/**
+	 * Initializes a game board for a given game, using a given board.
+	 * Useful for copying a game board.
+	 * @param game The game. You lost it.
+	 * @param board A COPY of another game board.
+	 */
+	public Board(Game game, BoardSquare[][] board) {
+		this(game);
+		this.board = board;
 	}
 	
 	/**
-	 * Places a Tile on a specific BoardSquare of the game board.
-	 * @param tile The Tile to be placed.
-	 * @param x The x coordinate of the BoardSquare.
-	 * @param y The y coordinate of the BoardSquare.
+	 * Retrieves the BoardSquare on a specified position on the Board.
+	 * @param xcoord The x coordinate of the BoardSquare.
+	 * @param ycoord The y coordinate of the BoardSquare.
+	 * @return The BoardSquare on the specified position.
+	 * @throws SquareOutOfBoundsException When a coordinate is out of bounds.
 	 */
-	public void placeTile(Tile tile, int x, int y) {
-		// TODO Implement body.
+	public BoardSquare getSquare(int xcoord, int ycoord) throws SquareOutOfBoundsException {
+		int x = xcoord + (board.length / 2);
+		if (x >= board.length || x < 0) {
+			throw new SquareOutOfBoundsException(x, board.length, 'x');
+		}
+		int y = ycoord + (board[x].length / 2);
+		if (y >= board[x].length || y < 0) {
+			throw new SquareOutOfBoundsException(y, board[x].length, 'y');
+		}
+		return this.board[x][y];
 	}
 	
 	/**
-	 * Removes a Tile from a specific BoardSquare of the game board.
+	 * Places a Tile on a specific position on the game board.
 	 * @param tile The Tile to be placed.
-	 * @param x The x coordinate of the BoardSquare.
-	 * @param y The y coordinate of the BoardSquare.
+	 * @param x The x coordinate of the Tile.
+	 * @param y The y coordinate of the Tile.
+	 * @throws SquareOutOfBoundsException When a coordinate is out of bounds.
 	 */
-	public void removeTile(int x, int y) {
-		// TODO Implement body.
+	public void placeTile(Tile tile, int x, int y) throws SquareOutOfBoundsException {
+		this.getSquare(x, y).placeTile(tile);
+	}
+	
+	/**
+	 * Removes a Tile from a specific position on the game board.
+	 * @param tile The Tile to be placed.
+	 * @param x The x coordinate of the Tile.
+	 * @param y The y coordinate of the Tile.
+	 * @throws SquareOutOfBoundsException When a coordinate is out of bounds.
+	 */
+	public void removeTile(int x, int y) throws SquareOutOfBoundsException {
+		this.getSquare(x, y).removeTile();
 	}
 	
 	/**
 	 * Retrieves the Tile that is currently on a specific position on the Board.
-	 * @param x The x coordinate of the BoardSquare.
-	 * @param y The y coordinate of the BoardSquare.
+	 * @param x The x coordinate of the Tile.
+	 * @param y The y coordinate of the Tile.
 	 * @return The Tile on the specified position.
+	 * @throws SquareOutOfBoundsException When a coordinate is out of bounds.
 	 */
-	public Tile getTile(int x, int y) {
-		// TODO Implement body;
-		return null;
+	public Tile getTile(int x, int y) throws SquareOutOfBoundsException {
+		return this.getSquare(x, y).getTile();
 	}
 	
 	/**
-	 * Returns a copy of the board.
-	 * @return A copy of the board.
+	 * Returns a copy of the game board.
+	 * @return A copy of the game board.
 	 */
 	public Board copy() {
-		// TODO Implement body;
-		return null;
+		// We make a new 2D-array of board squares of the same size as our current board.
+		BoardSquare[][] boardCopy = new BoardSquare[this.board.length][this.board[0].length];
+		// We loop over every x-coordinate to copy every column.
+		for (int i = 0; i < this.board.length; i++) {
+			/*
+			 * We cannot just assign, because this would barely duplicate the address.
+			 * To truly copy, we use System.arraycopy on every column and assign this copy
+			 * to the new column in boardCopy.
+			 */
+			System.arraycopy(this.board[i], 0, boardCopy[i], 0, this.board[i].length);
+		}
+		// Finally, we construct a new Board using this copy of the board.
+		return new Board(this.game, boardCopy);
 	}
 	
 	/**
--- QuirkleGame/src/game/BoardSquare.java
+++ QuirkleGame/src/game/BoardSquare.java
@@ -1,5 +1,15 @@
 package game;
 
+import exceptions.SquareOutOfBoundsException;
+
+/**
+ * This class represents an layer between boards and tiles placed on these boards. It offers
+ * some extra functionality related to neighbours and more structured control of tiles placed
+ * on them.
+ * 
+ * @author Jonathan Juursema & Peter Wessels
+ *
+ */
 public class BoardSquare {
 	
 	public static final int NORTH = 0;	
@@ -8,37 +18,70 @@ public class BoardSquare {
 	public static final int WEST = 3;  
 
 	private Tile tile;
+	private Board board;
 	
 	private int x;
 	private int y;
 	
 	/**
 	 * Construct an empty BoardSquare in a given position.
+	 * @param board The game board.
 	 * @param x The x position.
 	 * @param y The y position.
 	 */
-	public BoardSquare(int x, int y) {
-		// TODO Implement body.
+	public BoardSquare(Board board, int x, int y) {
+		this(board, x, y, null);
 	}
 	
 	/**
 	 * Constructs an empty BoarDSquare in a given position and place a Tile on it.
+	 * @param board The game board.
 	 * @param x The x position.
 	 * @param y The y position.
 	 * @param tile The Tile.
 	 */
-	public BoardSquare(int x, int y, Tile tile) {
-		// TODO Implement body.
+	public BoardSquare(Board board, int x, int y, Tile tile) {
+		this.x = x;
+		this.y = y;
+		this.tile = tile;
+		this.board = board;
+	}
+	
+	/**
+	 * Places a Tile on this BoardSquare.
+	 * @param tile The Tile to be placed.
+	 */
+	public void placeTile(Tile theTile) {
+		this.tile = theTile;
+	}
+	
+	/**
+	 * Removes the tile from this BoardSquare.
+	 */
+	public void removeTile() {
+		this.tile = null;
 	}
 	
 	/**
 	 * Returns a BoardSquare next to this BoardSquare in the specified direction.
-	 * @param direction
-	 * @return
+	 * @param A direction. See the public static integers of this class.
+	 * @return The BoardSquare next to the current BoardSquare in the given direction.
+	 * @throws SquareOutOfBoundsException When a coordinate is out of range. 
+	 * This should never happen though.
 	 */
-	public BoardSquare getNeighbour(int direction) {
-		// TODO Implement body;
-		return null;
+	public BoardSquare getNeighbour(int direction) throws SquareOutOfBoundsException {
+		switch (direction) {
+			case BoardSquare.NORTH:
+				return this.board.getSquare(this.x + 1, this.y);
+			case BoardSquare.EAST:
+				return this.board.getSquare(this.x, this.y + 1);
+			case BoardSquare.SOUTH:
+				return this.board.getSquare(this.x - 1, this.y);
+			case BoardSquare.WEST:
+				return this.board.getSquare(this.x, this.y - 1);
+			default:
+				return null;
+		}
 	}
 	
 	/**
@@ -46,8 +89,15 @@ public class BoardSquare {
 	 * @return True when there is a Tile placed, false otherwise.
 	 */
 	public boolean isEmpty() {
-		// TODO Implement body.
-		return false;
+		return this.tile == null;
+	}
+	
+	/**
+	 * Returns the tile on this BoardSquare.
+	 * @return The Tile on this BoardSquare.
+	 */
+	public Tile getTile() {
+		return this.tile;
 	}
 
 	/**
@@ -61,16 +111,7 @@ public class BoardSquare {
 	 * Returns this BoardSquare's y position.
 	 * @return The y position.
 	 */
-	public int getY() {
+ 	public int getY() {
 		return this.y;
-	}
-	
-	/**
-	 * Returns the tile on this BoardSquare
-	 * @return The Tile on this BoardSquare
-	 */
-	
-	public Tile getTile() {
-		return this.tile;
 	}
 }
