87aad15d7ac0b71766f34c1fa62189730d3c1edf
MrFix93 <pjotrwes@hotmail.com>
2016-01-15 11:55:26 +0100

    Merge commit checkout conflict

--- QuirkleGame/src/test/TestTurn.java
+++ QuirkleGame/src/test/TestTurn.java
@@ -29,17 +29,30 @@ public class TestTurn {
 		bag.fill();
 		Player player = new Player("Peter", "");
 		Turn turn = new Turn(player, board);
+		
 		Tile tile1 = new Tile(Tile.RED,Tile.CIRCLE);
-		Tile tile2 = new Tile(Tile.BLUE,Tile.SQUARE);
+		Tile tile2 = new Tile(Tile.RED,Tile.CIRCLE);
+		Tile tile3 = new Tile(Tile.RED,Tile.CIRCLE);
+		Tile tile4 = new Tile(Tile.RED,Tile.CIRCLE);
+
 		BoardSquare square1 = board.getSquare(0, 1);
 		BoardSquare square2 = board.getSquare(0, 2);
+		BoardSquare square3 = board.getSquare(1, 1);
+		BoardSquare square4 = board.getSquare(1, 2);
+		
+		board.placeTile(tile3, square3.getX(), square3.getY());
+		board.placeTile(tile4, square4.getX(), square4.getY());
+
 		Move move1 = new Move(tile1, square1);
 		Move move2 = new Move(tile2, square2);
 		
 		turn.addMove(move1);
 		turn.addMove(move2);
 		
-		turn.toString();
+		System.out.println(turn.toString());
+		
+		System.out.println(turn.getScore());
 	}
 
 }
+
